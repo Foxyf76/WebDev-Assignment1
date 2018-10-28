d15e758e5dc1d2022295b265bc24e76d2fa1321c
Foxyf76 <lukefox5@gmail.com>
2018-10-28 16:36:02 +0000

    Fix route

--- models/user_accounts.js
+++ models/user_accounts.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let UserSchema = new mongoose.Schema({
         username: {type: String, required: true},
         email: {type: String, required: true},
-        password: {type: String, required: true},
+        password: {type: String, required: true}
        // products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }] <--- Not working, needs to be implemented in assignment 2
     },
     {collection: 'useraccountdb'});
