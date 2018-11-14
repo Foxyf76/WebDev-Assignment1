a4c0a218fa26ae9c82618dcfae996fd20eb5f3f8
Foxyf76 <lukefox5@gmail.com>
2018-11-14 12:54:49 +0000

    Update cors

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var usersRouter = require('./routes/users');
 var cors = require('cors');
 
 const products = require("./routes/products");
-const productspecs = require("./routes/product_specs");
 const useraccounts = require("./routes/user_accounts");
 
 var app = express();
@@ -21,41 +20,44 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(cors());
+
+///''''GET''''///
 app.get('/products', products.findAll);
 app.get('/products/votes', products.findTotalVotes);
 app.get('/products/:id', products.findOne);
-app.get('/products/getByName/:productname/', products.findProductSpecs); // not working
-app.get('/fuzzy/:productname', products.findFuzzy);
+app.get('/products/getByName/:productname', products.findProductByName);
+app.get('/fuzzy/:productname', products.findFuzzyName);
 app.get('/useraccounts', useraccounts.findAll);
 
-app.get('/productspecs', productspecs.findAll);
+///''''POST''''///
+app.post('/products/addProduct', products.addProduct);
+app.post('/products/:id/addSpecs', products.addSpecs);
+app.post('/useraccounts/addUser', useraccounts.addUser);
 
-app.post('/products',products.addProduct);
-app.post('/productspecsadd',productspecs.addSpecs);
-
-
-
+///''''PUT''''///
 app.put('/products/:id/vote', products.incrementUpvotes);
+app.put('/products/:id/downvote', products.downvote);
+app.put('/useraccounts/:username/', useraccounts.changeUsername);
 
-app.delete('/products/:id', products.deleteProduct);
-
-
+///''''DELETE''''///
+app.delete('/products/delete/:id', products.deleteProduct);
+app.delete('/useraccounts/delete/:id', useraccounts.deleteUser);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
